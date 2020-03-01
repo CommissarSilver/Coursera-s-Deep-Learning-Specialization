2039d7b8f41aae6e0bcc6f1a10ef68962dc8ab0a
Ahura Majdinasab <fardaar54@gmail.com>
2020-03-01 08:29:47 +0330

    conv_backward function

--- Convolutional Neural Networks/model.py
+++ Convolutional Neural Networks/model.py
@@ -74,3 +74,36 @@ def pool_forward(A_prev, hyper_parameters, mode='max'):
 
     cache = (A_prev, hyper_parameters)
     return A, cache
+
+def conv_backward(dZ,cache):
+    (A_prev,W,b,hyper_parameters)=cache
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+    (f, f, n_C_prev, n_C) = W.shape
+    stride = hyper_parameters["stride"]
+    pad = hyper_parameters["pad"]
+
+    (m, n_H, n_W, n_C) = dZ.shape
+    dA_prev = np.zeros((m, n_H_prev, n_W_prev, n_C_prev))
+    dW = np.zeros((f, f, n_C_prev, n_C))
+    db = np.zeros((1, 1, 1, n_C))
+    A_prev_pad = zero_pad(A_prev, pad)
+    dA_prev_pad = zero_pad(dA_prev, pad)
+
+    for i in range(m):
+        a_prev_pad = A_prev_pad[i]
+        da_prev_pad = dA_prev_pad[i]
+        for h in range(n_H):
+            for w in range(n_W):
+                for c in range(n_C):
+                    vert_start = h * stride
+
+                    vert_end = vert_start + f
+                    horiz_start = w * stride
+
+                    horiz_end = horiz_start + f
+                    a_slice = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
+                    da_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :] += W[:, :, :, c] * dZ[i, h, w, c]
+                    dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
+                    db[:, :, :, c] += dZ[i, h, w, c]
+        dA_prev[i, :, :, :] = da_prev_pad[pad:-pad, pad:-pad, :]
+    return dA_prev, dW, db
